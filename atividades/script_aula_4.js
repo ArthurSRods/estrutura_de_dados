01dfb43762af5e7d62fb9bc9242e7720774e2c0d
ArthurSRods <arthur.srodrigues@hotmail.com>
2025-09-10 10:39:12 -0300

    Criação do repositorio

---
+++ atividades/script_aula_4.js
@@ -0,0 +1,18 @@
+// Arrays
+// let daysOfWeek = new Array ();
+
+// let daysOfWeek = new Array (7);
+
+// let daysOfWeek = new Array ('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday');
+
+// let daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+// let daysOfWeekPop = daysOfWeek.pop()
+
+// console.log(daysOfWeek)
+// console.log(daysOfWeek[1])
+// console.log(daysOfWeekPop)
+
+// console.table(daysOfWeek)
+
+
+let numbers = new Array [1, 3, 4, 5, 6, 89]
